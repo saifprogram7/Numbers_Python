8447267c53076c7e7ec162a1fb0e744170bfb9b2
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-05-29 11:36:11 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,24 @@
+from math import *
+#Printing Float Value
+floatnumber = 12.56
+print(floatnumber)
+#Adding Value
+firstnumber = 5
+secondnumber = 7
+result = firstnumber + secondnumber
+print(result)
+#Substracting Value
+minusfirstnumber = 8
+minussecondnumber = 3
+results = minusfirstnumber - minussecondnumber
+print(results)
+#Multiply Value
+multiplyfirstnumber = 7
+multiplysecondnumber = 7
+multiplyresult = multiplyfirstnumber * multiplysecondnumber
+print(multiplyresult)
+#Dividing Value
+dividefirstnumber = 72
+dividebynumber = 2
+divideresult = int(dividefirstnumber / dividebynumber)
+print(divideresult)
